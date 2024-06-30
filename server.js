2abdd7c0c27c4f6da42af3296233b925be32b29c
gurpreetsingh8792 <gurpreetsingh8792@gmail.com>
2024-06-30 22:09:44 +0530

    small changes

--- server.js
+++ server.js
@@ -495,7 +495,7 @@ app.delete('/users/delete', verifyToken, (req, res) => {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
